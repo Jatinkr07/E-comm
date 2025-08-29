03533e578045aaca1e7b61e87544ebc03747a858
Jatinkr07 <jatinkumar07911@gmail.com>
2025-08-29 23:07:09 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://e-comm-backend-ji6j.onrender.com";
 
 export default API_BASE_URL;
